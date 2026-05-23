ea29d79bfc808a965a152c179b9f6a88613ac1c4
Alexandre Böhs <alexandrekb25@gmail.com>
2026-05-22 21:23:49 -0300

    Remove legacy PHP files

--- delete.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include_once('config.php');
-if(isset($_REQUEST['delId']) and $_REQUEST['delId']!=""){
-	$db->delete('dadoscliente',array('id'=>$_REQUEST['delId']));
-	header('location: browse-users.php?msg=rds');
-	exit;
-}
-?>
